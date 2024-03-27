19a0e33522018e4db3f9380b635738cde8eadc6f
Henryle-hd <henrydionizi@gmail.com>
2024-03-27 21:47:27 +0300

    comment removed

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -8,14 +8,13 @@ import { Card, CardContent } from "./ui/card";
 import { Separator } from "./ui/separator";
 import Link from "next/link";
 import TypeWriterTitle from "./typewriter";
-import { useTheme } from "next-themes"
+import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
-import { ScrollArea } from "@/components/ui/scroll-area"
-
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const tags = Array.from({ length: 50 }).map(
-  (_, i, a) => `v1.2.0-beta.${a.length - i}`
-)
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`,
+);
 
 const otherLinks = [
   {
@@ -30,16 +29,14 @@ const otherLinks = [
     title: "CBE library",
     link: "/",
   },
-]
+];
 
 function AsideComponent() {
   const { theme, setTheme } = useTheme();
 
-
-
   return (
     <>
-      <Card className="sticky left-0 top-0 flex min-h-screen w-full flex-col  justify-start gap-4 rounded-r-xl rounded-l-none px-1">
+      <Card className="sticky left-0 top-0 flex min-h-screen w-full flex-col  justify-start gap-4 rounded-l-none rounded-r-xl px-1">
         <CardContent className="flex flex-col justify-start gap-4">
           {/* // cbe logo */}
           <div className="relative left-2 top-0 my-3 flex items-center justify-start gap-3">
@@ -74,7 +71,7 @@ function AsideComponent() {
                     key={tag}
                     variant="outline"
                     size={"lg"}
-                    className="text-sm w-full mb-2"
+                    className="mb-2 w-full text-sm"
                     onClick={() => console.log("clicked")}
                   >
                     {tag}
@@ -84,75 +81,33 @@ function AsideComponent() {
             </div>
           </ScrollArea>
 
-          <div className="flex flex-col justify-start gap-2 mt-4">
-            <h1 className="font-bold text-xl">other links</h1>
+          <div className="mt-4 flex flex-col justify-start gap-2">
+            <h1 className="text-xl font-bold">other links</h1>
             <div>
-              {
-                otherLinks.map((link) => (
-                  <Link
-                    key={link.title}
-                    href={link.link}
-                    className="flex items-center justify-start gap-2 text-cbeaiclr-1 text-sm"
-                  >
-                    <Link2 />
-                    {link.title}
-                  </Link>
-                ))
-              }
+              {otherLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.link}
+                  className="flex items-center justify-start gap-2 text-sm text-cbeaiclr-1"
+                >
+                  <Link2 />
+                  {link.title}
+                </Link>
+              ))}
             </div>
           </div>
 
-          {/* <div className=" max-h-96 min-h-96 w-full overflow-scroll rounded-lg border bg-white text-center text-cbeaiclr-4 shadow-md">
-            <ul className="gap grid grid-cols-1 scroll-smooth">
-              <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
-                List branchs of CBE and courses they offer
-              </li>
-              <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
-                Hello world list here!
-              </li>
-              <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
-                Hello world list here!
-              </li>
-              <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
-                Hello world list here!
-              </li>
-              <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
-                Hello world list here!
-              </li>
-              <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
-                Hello world list here!
-              </li>
-              <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
-                Hello world list here!
-              </li>
-              <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
-                Hello world list here!
-              </li>
-              <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
-                Hello world list here!
-              </li>
-              <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
-                Hello world list here!
-              </li>
-            </ul>
-          </div> */}
           <Separator />
           {/* // delete conversation */}
           <Button
             variant={"destructive"}
-            className="flex items-center justify-start gap-2">
+            className="flex items-center justify-start gap-2"
+          >
             <Trash />
             Delete conversation
           </Button>
           {/* //dark mode */}
 
-          {/* <Link href={"/"}>
-            <span className="flex items-center justify-start gap-2">
-              {" "}
-              <Moon />
-              Change themes
-            </span>
-          </Link> */}
           <Button
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
             variant="outline"
