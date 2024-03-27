730444d2944a715290986bc7d56be61852b5cf1c
passion studio <120102511+passion-steven28@users.noreply.github.com>
2024-03-27 16:53:49 +0300

    change the aside prompt

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -3,13 +3,35 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Input } from "./ui/input";
-import { Delete, DeleteIcon, Moon, Sun, Trash, Truck } from "lucide-react";
+import { Link2, Moon, Sun, Trash } from "lucide-react";
 import { Card, CardContent } from "./ui/card";
 import { Separator } from "./ui/separator";
 import Link from "next/link";
 import TypeWriterTitle from "./typewriter";
 import { useTheme } from "next-themes"
 import { Button } from "./ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area"
+
+
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`
+)
+
+const otherLinks = [
+  {
+    title: "CBE website",
+    link: "/",
+  },
+  {
+    title: "Cosis",
+    link: "/",
+  },
+  {
+    title: "CBE library",
+    link: "/",
+  },
+]
+
 function AsideComponent() {
   const { theme, setTheme } = useTheme();
 
@@ -44,7 +66,43 @@ function AsideComponent() {
           />
 
           {/* // container */}
-          <div className=" max-h-96 min-h-96 w-full overflow-scroll rounded-lg border bg-white text-center text-cbeaiclr-4 shadow-md">
+          <ScrollArea className="h-60 w-full rounded-md border">
+            <div className="p-2">
+              {tags.map((tag) => (
+                <>
+                  <Button
+                    key={tag}
+                    variant="outline"
+                    size={"lg"}
+                    className="text-sm w-full mb-2"
+                    onClick={() => console.log("clicked")}
+                  >
+                    {tag}
+                  </Button>
+                </>
+              ))}
+            </div>
+          </ScrollArea>
+
+          <div className="flex flex-col justify-start gap-2 mt-4">
+            <h1 className="font-bold text-xl">other links</h1>
+            <div>
+              {
+                otherLinks.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.link}
+                    className="flex items-center justify-start gap-2 text-cbeaiclr-1 text-sm"
+                  >
+                    <Link2 />
+                    {link.title}
+                  </Link>
+                ))
+              }
+            </div>
+          </div>
+
+          {/* <div className=" max-h-96 min-h-96 w-full overflow-scroll rounded-lg border bg-white text-center text-cbeaiclr-4 shadow-md">
             <ul className="gap grid grid-cols-1 scroll-smooth">
               <li className=" mx-5  mt-2 rounded-lg bg-cbeaiclr-5 p-4 shadow-sm">
                 List branchs of CBE and courses they offer
@@ -77,7 +135,7 @@ function AsideComponent() {
                 Hello world list here!
               </li>
             </ul>
-          </div>
+          </div> */}
           <Separator />
           {/* // delete conversation */}
           <Button
