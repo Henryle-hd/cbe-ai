07208680c9a5426a5d123013a4a683f1e668211a
Henryle-hd <henrydionizi@gmail.com>
2024-03-28 18:43:57 +0300

    navigation updated!

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -3,23 +3,27 @@ import React from "react";
 import { Button } from "./ui/button";
 import { ThemeToggle } from "./themeToggle";
 import TypeWriterTitle from "./typewriter";
+import { Separator } from "./ui/separator";
 
 export default function SmallScreen() {
   return (
-    <nav className="sticky top-0 z-50 flex w-[100%] items-center justify-between gap-2 rounded-xl bg-background p-2 px-4 shadow-md">
-      <div className="text-xl font-bold text-cbeaiclr-1">
-        <TypeWriterTitle />
-      </div>
-      <div className="flex items-center gap-2">
-        <ThemeToggle />
+    <>
+      <nav className="sticky top-0 z-50 flex w-[100%] items-center justify-between gap-2 rounded-xl bg-background p-2 px-4 shadow-md">
+        <div className="text-xl font-bold text-cbeaiclr-1">
+          <TypeWriterTitle />
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeToggle />
 
-        {/* <Button variant={"destructive"}>
+          {/* <Button variant={"destructive"}>
           <Trash />
         </Button> */}
-        <Button>
-          <Menu />
-        </Button>
-      </div>
-    </nav>
+          <Button>
+            <Menu />
+          </Button>
+        </div>
+      </nav>
+      <Separator />
+    </>
   );
 }
