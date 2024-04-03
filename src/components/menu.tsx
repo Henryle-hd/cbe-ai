24c72fca2ec1dd651706dd649ce68142c4a9c445
Henryle-hd <henrydionizi@gmail.com>
2024-04-04 02:14:53 +0300

    menu updated!

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -4,13 +4,26 @@ import { Button } from "./ui/button";
 import { ThemeToggle } from "./themeToggle";
 import TypeWriterTitle from "./typewriter";
 import { Separator } from "./ui/separator";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function SmallScreen() {
   return (
     <>
       <nav className="fixed top-0 z-50 flex w-[100%] items-center justify-between gap-2 rounded-xl bg-background p-2 px-4 shadow-md">
-        <div className="text-xl font-bold text-cbeaiclr-1">
-          <TypeWriterTitle />
+        <div className="flex items-center justify-center gap-2">
+          <Avatar className="p-1">
+            <AvatarImage
+              src="https://cosis.cbe.ac.tz//assets/themes/logo.png"
+              className=""
+            />
+            <AvatarFallback className=" font-bold text-cbeaiclr-1">
+              CBE
+            </AvatarFallback>
+          </Avatar>
+
+          <div className="text-xl font-bold text-cbeaiclr-1">
+            <TypeWriterTitle />
+          </div>
         </div>
         <div className="flex items-center gap-2">
           <ThemeToggle />
