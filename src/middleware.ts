744e7d8df4e22d6e257b1abe27d3789e132725cf
Henryle-hd <henrydionizi@gmail.com>
2024-05-18 09:24:47 +0300

    middleware updates!

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,16 @@
 import { NextResponse } from "next/server";
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
+// export default authMiddleware({
+//   // publicRoutes: ["((?!^/admn).*)"],
+//   publicRoutes: ["/", "/chat", "/chat/*", "/prompt", "/prompt/*"],
+// });
+
 export default authMiddleware({
   // publicRoutes: ["((?!^/admn).*)"],
-  publicRoutes: ["/", "/chat", "/chat/[^/]"],
+  publicRoutes: ["/", "/chat(/.*)?", "/prompt(/.*)?", "/api/chat"],
 });
+
 
 export const config = {
     matcher: [
