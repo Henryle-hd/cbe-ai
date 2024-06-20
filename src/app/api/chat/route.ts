ad9cbd4bd2cf8ed33aa89afdd6715fc195ae9be5
Henryle-hd <henrydionizi@gmail.com>
2024-06-20 08:37:38 +0300

    fix

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,26 +1,25 @@
 import { cbeinfoIndex } from "@/lib/db/pinecone";
 import prisma from "@/lib/db/prisma";
 import openai, { getCbeinfoEmbedding } from "@/lib/openia";
-import {OpenAIStream, StreamingTextResponse} from "ai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 import { ChatCompletionMessage } from "openai/resources/index.mjs";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export async function POST(req: Request) {
-    //getting system prompt from "/api/training"
-    const systemPromptResponse = await fetch(
-        'http://localhost:3000/api/training',
-    );
-    const systemPrompt = await systemPromptResponse.text();
-    console.log(systemPrompt)
+  //getting system prompt from "/api/training"
+  const systemPromptResponse = await fetch(
+    "https://cbe-ai.vercel.app/api/training",
+  );
+  const systemPrompt = await systemPromptResponse.text();
+  console.log(systemPrompt);
 
-
-    try {
+  try {
     const body = await req.json();
     const messages: ChatCompletionMessage[] = body.messages;
     const messageTruncated = messages.slice(-6);
 
     const embedding = await getCbeinfoEmbedding(
-        messageTruncated.map((message) => message.content).join("\n"),
+      messageTruncated.map((message) => message.content).join("\n"),
     );
 
     /*
@@ -31,33 +30,34 @@ export async function POST(req: Request) {
         chatbot: .........
         */
 
-      //searching
+    //searching
     const vectorQueryResponse = await cbeinfoIndex.query({
-        vector: embedding,
-        topK: 4,
+      vector: embedding,
+      topK: 4,
     });
 
-      //getting text info
+    //getting text info
     const relevantCbeInfo = await prisma.info.findMany({
-        where: {
+      where: {
         id: {
-            in: vectorQueryResponse.matches.map((match) => match.id),
+          in: vectorQueryResponse.matches.map((match) => match.id),
         },
-        },
+      },
     });
 
-      //testing
+    //testing
     console.log("Relevant CBE info:", relevantCbeInfo);
 
     const systemMessage: ChatCompletionMessage = {
-        role: "assistant",
-        content: systemPrompt+
+      role: "assistant",
+      content:
+        systemPrompt +
         "The relevant CBE inforamation for this query are: \n" +
         relevantCbeInfo
-            .map(
+          .map(
             (info) => `Title ${info.title}\n\nMain body:\n ${info.main_body}`,
-            )
-            .join("\n\n"),
+          )
+          .join("\n\n"),
     };
 
     /* sample of above code:
@@ -66,24 +66,18 @@ export async function POST(req: Request) {
         Dar es Salaam College of Busineess .........
         */
 
-        //Request to openai(chatGPT)
-        const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            stream: true,
-            messages: [systemMessage, ...messageTruncated],
-        });
+    //Request to openai(chatGPT)
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages: [systemMessage, ...messageTruncated],
+    });
 
-
-        //return the result to fontEnd
-        const stream = OpenAIStream(response);
-        return new StreamingTextResponse(stream);
-
-
-    } catch (error) {
-        console.error(error);
-        return Response.json(
-            { error: "Internal server error" },
-            { status: 500 },
-        );
-    }
+    //return the result to fontEnd
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
 }
